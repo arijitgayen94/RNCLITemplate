82591bad36ef6b653f6eaa131414d53182a7cc4a
arijitgayen94 <arijitgayen45@gmail.com>
2025-10-03 17:31:44 +0530

    Create template

---
+++ template.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // Placeholder used to rename and replace in files
+  // package.json, index.json, android/, ios/
+  placeholderName: "arijitgayen94",
+
+  // Directory with template
+  templateDir: "./template",
+
+  // Path to script, which will be executed after init
+  postInitScript: "./script.js",
+  titlePlaceholder: "@arijitgayen94 RNCLITemplate",
+}
